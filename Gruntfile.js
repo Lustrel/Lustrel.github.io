7b53b5679908e95a86814a5ce06ba33a25e90390
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-08-30 14:04:24 -0300

    Convertido indentacao de todos os arquivos de espaco para tab.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,69 @@
 "use strict";
 
 module.exports = function(Grunt) {
-    const bowerPath = "./bower_components";
-    const sourcePath = "./src";
-    const distPath = "./dist";
-    const distCssPath = (distPath + "/css");
-    const distJsPath = (distPath + "/js");
+	const bowerPath = "./bower_components";
+	const sourcePath = "./src";
+	const distPath = "./dist";
+	const distCssPath = (distPath + "/css");
+	const distJsPath = (distPath + "/js");
 
-    Grunt.initConfig({
+	Grunt.initConfig({
 
-        // Concat
-        concat: {
-            options: { separator: "; \n" },
-            vendor: {
-                src: [
-                    (bowerPath + "/jquery/dist/jquery.min.js"),
-                    (bowerPath + "/angular/angular.min.js"),
-                    (bowerPath + "/angular-route/angular-route.min.js")
-                ],
-                dest: (distJsPath + "/vendor.js")
-            },
-            app: {
-                src: [
-                    (sourcePath + "/app.module.js")
-                ],
-                dest: (distJsPath + "/app.js")
-            }
-        },
-        
-        // Concat CSS
-        concat_css: {
-            vendor: {
-                src: [
-                    (bowerPath + "/reset-css/reset.css"),
-                ],
-                dest: (distCssPath + "/vendor.css")
-            },
-            app: {
-                src: [
-                    (sourcePath + "/pages/home/home.style.css")
-                ],
-                dest: (distCssPath + "/app.css")
-            }
-        },
+		// Concat
+		concat: {
+			options: { separator: "; \n" },
+			vendor: {
+				src: [
+					(bowerPath + "/jquery/dist/jquery.min.js"),
+					(bowerPath + "/angular/angular.min.js"),
+					(bowerPath + "/angular-route/angular-route.min.js")
+				],
+				dest: (distJsPath + "/vendor.js")
+			},
+			app: {
+				src: [
+					(sourcePath + "/app.module.js")
+				],
+				dest: (distJsPath + "/app.js")
+			}
+		},
+		
+		// Concat CSS
+		concat_css: {
+			vendor: {
+				src: [
+					(bowerPath + "/reset-css/reset.css"),
+				],
+				dest: (distCssPath + "/vendor.css")
+			},
+			app: {
+				src: [
+					(sourcePath + "/pages/home/home.style.css")
+				],
+				dest: (distCssPath + "/app.css")
+			}
+		},
+		
+		copy: {
+			components_templates: {
+				expand: true,
+				cwd: (sourcePath + "/components"),
+				// src: "**/*.html",
+				dest: (distPath + "/templates/components")
+			},
+			pages_templates: {
+				expand: true,
+				cwd: (sourcePath + "/pages"),
+				src: "**/*.html",
+				dest: (distPath + "/templates/pages")
+			}
+		},
+	});
 
-    });
+	Grunt.loadNpmTasks("grunt-contrib-concat");
+	Grunt.loadNpmTasks("grunt-concat-css");
 
-    Grunt.loadNpmTasks("grunt-contrib-concat");
-    Grunt.loadNpmTasks("grunt-concat-css");
-    
-    Grunt.registerTask("default", ["concat:app", "concat_css:app"]);
-    Grunt.registerTask("vendor", ["concat:vendor", "concat_css:vendor"]);
-    Grunt.registerTask("build", ["vendor", "default"]);
+	Grunt.registerTask("default", ["concat:app", "concat_css:app"]);
+	Grunt.registerTask("vendor", ["concat:vendor", "concat_css:vendor"]);
+	Grunt.registerTask("build", ["vendor", "default"]);
 };
--- src/app.module.js
+++ src/app.module.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular
-    .module('Lustrel', ['ngRoute'])
-    .config(function($routeProvider){
-        $routeProvider
-            .when('/', { 
-                templateUrl: './src/pages/home/home.template.html'
-            })
-            .when('/contact', {
-                templateUrl: './src/pages/contact/contact.template.html'
-            })
-    });
+	.module('Lustrel', ['ngRoute'])
+	.config(function($routeProvider){
+		$routeProvider
+			.when('/', { 
+				templateUrl: './src/pages/home/home.template.html'
+			})
+			.when('/contact', {
+				templateUrl: './src/pages/contact/contact.template.html'
+			})
+	});
